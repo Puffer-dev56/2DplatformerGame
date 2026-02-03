b2e068fdf0f881672fa98fbb7812b247ba888bb4
Puffer-dev56 <pongsapat4848@gmail.com>
2026-02-03 16:54:01 +0700

    Add function
    - player doble jump
    
    - plyer action when take damage
    
    - Restart when player die

--- Player.cs
+++ Player.cs
@@ -1,7 +1,14 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //heath
+    public int health;
+    private SpriteRenderer spriteRenderer;
+    private int knockback = 100;
+
     //player walk
     private Rigidbody2D rb;
     public int speed;
@@ -13,6 +20,8 @@ public class Player : MonoBehaviour
     public Transform CheckGround;
     public LayerMask GroundLayer;
     public float CheckGroundRadius;
+    public int extraJump = 1;
+    private int deExtraJump;
 
     //player Animation
     private Animator animator;
@@ -22,6 +31,8 @@ public class Player : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        deExtraJump = extraJump;
     }
 
     // Update is called once per frame
@@ -30,8 +41,16 @@ public class Player : MonoBehaviour
         moveX = Input.GetAxis("Horizontal");
         rb.linearVelocity = new Vector2(moveX * speed, rb.linearVelocity.y);
 
-        if (Input.GetButtonDown("Jump") && isGrounded) {
-            rb.AddForce(new Vector2(rb.linearVelocity.x, jumpForce));
+        if (Input.GetButtonDown("Jump")) {
+
+            if (isGrounded) {
+                rb.AddForce(new Vector2(rb.linearVelocity.x, jumpForce));
+                deExtraJump = extraJump;
+            }
+            else if(deExtraJump > 0) {
+                rb.AddForce(new Vector2(rb.linearVelocity.x, jumpForce));
+                deExtraJump--;
+            }
         }
         SetAnimation();
     }
@@ -60,5 +79,28 @@ public class Player : MonoBehaviour
                 animator.Play("player_fall");
             }
         }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Damage")) {
+            health -= 1;
+            rb.AddForce(new Vector2(rb.linearVelocity.y, knockback));
+            StartCoroutine(BlinkRed());
+
+            if (health <= 0) {
+                Die();
+            }
+        }
+    }
+
+    private void Die()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+    }
+
+    private IEnumerator BlinkRed() { 
+        spriteRenderer.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        spriteRenderer.color = Color.white;
     }
 }
